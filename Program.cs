ad797e85e8c8abedbb092af1b381053db709883d
Francis3707 <f.vidalpalta2017@gmail.com>
2025-08-29 14:27:49 -0400

    POO3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿public class Coche
 {
-    
+    public string? Marca { get; set; }
+    public decimal? Velocidad { get; set; }
+    //crear el constructor
+    public Coche(string marca, decimal velocidad);
+
 }
